d878fad3712548dd7688e88aa079ff8837b61792
songjia <976166230@qq.com>
2020-12-24 14:00:51 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -56,4 +56,42 @@ class UsersController extends Controller
     }
 
 
+    /**
+     * 用户编辑显示
+     * @author 宋佳
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+
+
+    /***
+     * 用户编辑请求
+     * @author 宋佳
+     * @param User $user
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(User $user,Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
+    }
+
 }
