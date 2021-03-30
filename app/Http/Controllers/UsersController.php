281df41ffb05d428f90a76009617918e2a53026f
songjia <songjia774@sohu.com>
2021-03-30 14:29:28 +0800

    浏览器缓存问题

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -65,7 +65,7 @@ class UsersController extends Controller
      */
     public function store(Request $request){
         $this->validate($request,[
-            'name' => 'required|unique:users|max:50',
+            'name1' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
